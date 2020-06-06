8e08f32a646f40e9f8ef0293d9baad8d7d287570
kinakome <ryomatsu0120@gmail.com>
2020-06-06 12:31:49 +0900

    Add circleCI config

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,6 +39,19 @@ module.exports = {
   */
   modules: [
   ],
+
+  // setting dotenv
+  modules: [
+    '@nuxtjs/dotenv'
+  ],
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASEURL: process.env.FIREBASE_DATABASEURL,
+    FIREBASE_PROJECTID: process.env.FIREBASE_PROJECTID,
+    FIREBASE_STORAGEBUCKET: process.env.FIREBASE_STORAGEBUCKET,
+  },
+
   /*
   ** Build configuration
   */
