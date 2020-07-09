6cf4109bd6c1fad5131af3e0d311145cf9aa01f2
kinakome <ryomatsu0120@gmail.com>
2020-07-09 20:43:46 +0900

    [Update] app title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Daily Necessities',
+    // title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no' },
