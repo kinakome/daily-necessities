d249280577934acfcc1e349ba87a71b9faf13806
kinakome <ryomatsu0120@gmail.com>
2020-07-14 21:06:27 +0900

    [Add] Station marker

--- pages/map.vue
+++ pages/map.vue
@@ -5,11 +5,13 @@
         :zoom="zoom"
         :center="center"
         :options="{zoomControl: true}"
-        v-if="getLocation"
+        v-if="getLocation && stations.length !=0"
       >
         <l-tile-layer :url="url"></l-tile-layer>
-        <l-marker :lat-lng="[35.531424, 139.698249]"></l-marker>
-        <!-- <l-marker v-for="marker of marker" :lat-lng="marker"></l-marker> -->
+        <l-marker :lat-lng="center"></l-marker>
+        <l-marker v-for="station of stations" :lat-lng="[station.location.latitude, station.location.longitude]" 
+          :key="station.name" 
+        ></l-marker>
       </l-map>
       <vue-loading type="spin" color="#333" :size="{ width: '100px', height: '100px' }" v-else></vue-loading>
     </div>
@@ -29,20 +31,42 @@ export default {
       url: "https://{s}.tile.osm.org/{z}/{x}/{y}.png",
       zoom: 17,
       center: [this.$store.state.location.latitude, this.$store.state.location.longitude],
-      marker: {
-        center:  [this.$store.state.location.latitude, this.$store.state.location.longitude]
-      }
+      stations: {},
+      marker: {}
     }
   },
   computed: {
     getLocation() {
       return this.$store.getters['getLocation']
+    },
+    getStation() {
+      return this.$store.getters['trainInfo/stations']
     }
   },
   watch: {
     getLocation(newStatus) {
       this.center = [this.$store.state.location.latitude, this.$store.state.location.longitude]
       this.marker.center = [this.$store.state.location.latitude, this.$store.state.location.longitude]
+    },
+    getStation(newStatus) {
+      let stations = []
+      //駅名の重複排除
+      const names = newStatus.map(item => item.name).filter((value, index, self) => self.indexOf(value) == index)
+      //同じ駅名の路線を集約してハッシュを作成
+      names.forEach(staName => {
+        let tmpStation = newStatus.find(({name}) => name === staName)
+        var stationInfo = {
+          name: staName,
+          train: newStatus.filter(({name}) => name === staName),
+          location: {
+            latitude: tmpStation.y,
+            longitude: tmpStation.x
+          }
+        }
+        stations.push(stationInfo)
+      })
+
+      this.stations = stations
     }
   }
 }
